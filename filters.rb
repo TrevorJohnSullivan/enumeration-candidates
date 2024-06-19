a6600cf1408e2e2256dd500dcb1ac38f56204087
Trevor Sullivan <trevorjohnsullivan@gmail.com>
2024-06-19 17:06:49 -0230

    update

--- filters.rb
+++ filters.rb
@@ -2,15 +2,45 @@
 # This way, we keep these methods separated from other potential parts of the program
 
 def find(id)
-    # Your code Here
+  @candidates.find { |candidate| candidate[:id] == id }
   end
   
   def experienced?(candidate)
-    # Your code Here
+    candidate[:years_of_experience] >= 2
   end
   
   def qualified_candidates(candidates)
-    # Your code Here
+    candidates.select do |candidate|
+      experienced?(candidate) &&
+      has_enough_github_points?(candidate) &&
+      knows_required_languages?(candidate) &&
+      applied_recently?(candidate) &&
+      age_valid?(candidate)
+    end
   end
   
-  # More methods will go below
+  def experienced?(candidate)
+    candidate[:years_of_experience] >= 2
+  end
+  
+  def has_enough_github_points?(candidate)
+    candidate[:github_points] >= 100
+  end
+  
+  def knows_required_languages?(candidate)
+    (candidate[:languages] & ['Ruby', 'Python']).any?
+  end
+  
+  def applied_recently?(candidate)
+    candidate[:date_applied] >= 15.days.ago.to_date
+  end
+  
+  def age_valid?(candidate)
+    candidate[:age] > 17
+  end
+
+  def ordered_by_qualifications(candidates)
+    candidates.sort_by do |candidate|
+      [-candidate[:years_of_experience], -candidate[:github_points]]
+    end
+  end
--- main.rb
+++ main.rb
@@ -4,6 +4,22 @@ require 'pp'
 require './candidates'
 require './filters'
 
-## Your test code can go here
-
+# Print all candidates
+puts "All Candidates:"
 pp @candidates
+
+# Find a specific candidate by id
+candidate_id = 5
+found_candidate = find(candidate_id)
+puts "\nCandidate with id #{candidate_id}:"
+pp found_candidate
+
+# List all qualified candidates
+puts "\nQualified Candidates:"
+qualified = qualified_candidates(@candidates)
+pp qualified
+
+# List all candidates ordered by qualifications
+puts "\nCandidates ordered by qualifications:"
+ordered_candidates = ordered_by_qualifications(@candidates)
+pp ordered_candidates
